4649b345971fdf5bcde13936c70cee8acc0df226
Shrey <shreyas-srish@student.kirkwood.edu>
2019-02-28 17:53:33 -0800

    Update PassengerSeat.java

--- src/PassengerSeat.java
+++ src/PassengerSeat.java
@@ -1,3 +1,4 @@
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -5,12 +6,15 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import java.util.Scanner;
+
 public class PassengerSeat {
 	public static void main(String[] args) {
-		String str = "1A 3B 2C 2A 3B 50J 45A 50A 50B 50C 9A 50D 50E 50F 50G 50H 50K 1B 1C 1E 1F 1G";
-		/*Scanner input = new Scanner(System.in);
+		// String str = "1A 3B 2C 2A 50J 50A 50B 50C 50D 50E 50F 50G 50H 50K 1B
+		
+		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the reserved seats: ");
-		String str = input.nextLine();*/
+		String str = input.nextLine();
 		System.out.println(checkSeats(str));
 	}
 
@@ -18,59 +22,84 @@ public class PassengerSeat {
 
 		// Declaration based on assumptions mentioned in problem
 		int numberRows = 50;
-		int perRow = 10;
 		int maxThree = numberRows * 3;
-		int remainingMaxThree = 0;
+		int remainingMaxThree = maxThree;
+		int threeSeat = 0;
 
 		String upperCase = string.toUpperCase();
-		
-		/* Associating rows A through K with different number of zeros to
-		 identify them after sorting*/
+
+		/*
+		 * Associating rows A through K with different number of zeros to
+		 * identify them after sorting
+		 */
 		upperCase = upperCase.replace("A", "00");
 		upperCase = upperCase.replace("B", "00");
 		upperCase = upperCase.replace("C", "00");
-		upperCase = upperCase.replace("D", "000");
-		upperCase = upperCase.replace("E", "000");
-		upperCase = upperCase.replace("F", "000");
-		upperCase = upperCase.replace("G", "000");
-		upperCase = upperCase.replace("H", "00000");
-		upperCase = upperCase.replace("J", "00000");
-		upperCase = upperCase.replace("K", "00000");
+		upperCase = upperCase.replace("D", "0000");
+		upperCase = upperCase.replace("E", "0000");
+		upperCase = upperCase.replace("F", "0000");
+		upperCase = upperCase.replace("G", "0000");
+		upperCase = upperCase.replace("H", "000000");
+		upperCase = upperCase.replace("J", "000000");
+		upperCase = upperCase.replace("K", "000000");
 
 		upperCase = upperCase.replace(" ", ",");
 
 		Map<Integer, Integer> repeated = new HashMap<>();
-		
-		//Converting the string to a list of integers
+
+		// Converting the string to a list of integers
 		List<Integer> seats = Stream.of(upperCase.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+
 		Collections.sort(seats);
 
-		// System.out.println(seats);
-		
-		//Enhanced for loop to populate HashMap with repeated values
+		System.out.println(seats);
+		// System.out.println(seats.size());
+
+		// Enhanced for loop to populate HashMap with repeated values
 		for (int number : seats) {
 			int counter = repeated.containsKey(number) ? repeated.get(number) : 0;
 			counter += 1;
 			repeated.put(number, counter);
 		}
-		
-		//Enhanced for loop to calculate number of triplicates 
-		int threeSeat = 0;
+		System.out.println(repeated);
+
+		// Enhanced for loop to track number of zeros
+		for (int tracker : repeated.keySet()) {
+			int store = tracker;
+			int counter = 0;
+			while (store % 10 == 0) {
+				counter++;
+				store /= 10;
+				// System.out.println(counter);
+			}
+			System.out.println(counter);
+			if ((counter == 4 || counter == 5) && repeated.get(tracker) >= 2) {
+				remainingMaxThree -= 1;
+			}
+			// System.out.println(counter);
+
+			if ((counter == 2 || counter == 3) && repeated.get(tracker) >= 1) {
+				remainingMaxThree -= 1;
+			}
+			if ((counter == 6 || counter == 7) && repeated.get(tracker) >= 1) {
+				remainingMaxThree -= 1;
+			}
+
+		}
+
+		// Loop to find number of triplicates
 		for (int getCtr : repeated.values()) {
 			if (getCtr > 2) {
 
 				threeSeat += getCtr / 3;
 
 			}
+
 		}
 
 		/*
-		 * ABC [||] DEFG [||] HJK 
-		 * ABC [||] DEFG [||] HJK
+		 * ABC [||] DEFG [||] HJK ABC [||] DEFG [||] HJK
 		 */
-		
-		
-		remainingMaxThree = maxThree - threeSeat;
 
 		System.out.println("Maximum 3 Person family that can be seated: " + maxThree);
 		System.out.println();
